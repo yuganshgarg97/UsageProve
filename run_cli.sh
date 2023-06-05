2cbd6c2e6859ef3b07fef36b37cebfcc0dda170f
yuganshgarg97 <Yuganshgarg97@gmail.com>
2023-06-05 14:16:44 +0530

    Add files via upload

---
+++ run_cli.sh
@@ -0,0 +1,5 @@
+python3 generate_data.py
+python3 generate-date-web.py
+python3 main.py
+python3 plot_data_cli.py
+
---
+++ run_gui.sh
@@ -0,0 +1,3 @@
+python3 generate_data.py
+python3 main.py
+python3 plot_data_gui.py
